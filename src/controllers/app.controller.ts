4dd2eeadd66f80b2fe3b2a93ba06569b34614b50
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-01-23 18:06:12 +0100

    l

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
+  @Get()
+  root() {
+    return 'ScorecastAPI v1.0.0';
+  }
+
   @UseGuards(AuthGuard('local'))
-  @Post('auth/login')
-  async login(@Request() req) {
-    return req.user;
+  @Post('/login')
+  async login(@Request() request): Promise<any> {
+    return request.user;
   }
 }
